dc10c4f41674f575fca89ce59e36097b1ecf4f9d
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-04-25 22:25:43 +0000

    End the game if either players wins 5 rounds

--- index.js
+++ index.js
@@ -43,9 +43,12 @@ let computerScore = 0;
 let buttons = [rockBtn, paperBtn, scissorsBtn];
 buttons.forEach((button) => button.setAttribute('type', 'button'));
 buttons.forEach((button) => subContainer.appendChild(button));
-buttons.forEach((button) => button.addEventListener('click', () => {
-    playRound(button.getAttribute('class'));
-}));
+
+let getPlayerSelection = (e) => {
+    playRound(e.target.getAttribute('class'));
+}
+
+buttons.forEach((button) => button.addEventListener('click', getPlayerSelection));
 
 clearBtn.setAttribute('type', 'reset');
 clearBtn.addEventListener('click', restartGame);
@@ -55,45 +58,54 @@ displayScore(playerScore, computerScore);
 
 // play one round of rock-paper-scissors
 function playRound(playerSelection, computerSelection = computerPlay()) {
-    if (playerScore === 5) return result.textContent = "Game Over! You won";
-    if (computerScore === 5) return result.textContent = "Game Over! You lost";
-
     if (playerSelection === computerSelection) return result.textContent = 'Phew,a tie!';
     
     if (playerSelection === options[0] && computerSelection === options[1]) {
         displayScore(playerScore, ++computerScore);
-        return result.textContent = "you lost, rock < paper";
+        return endGame() || (result.textContent = "you lost, rock < paper");
     }
     
     if (playerSelection === options[0] && computerSelection === options[2]) {
         displayScore(++playerScore, computerScore);
-        return result.textContent = "You won, rock > scissors";
+        return endGame() || (result.textContent = "You won, rock > scissors");
     }
     
     if (playerSelection === options[1] && computerSelection === options[0]) {
         displayScore(++playerScore, computerScore);
-        return result.textContent = "You won, paper > rock";
+        return endGame() || (result.textContent = "You won, paper > rock");
     }
     
     if (playerSelection === options[1] && computerSelection === options[2]) {
         displayScore(playerScore, ++computerScore);
-        return result.textContent = "You lost, paper < scissors";
+        return endGame() || (result.textContent = "You lost, paper < scissors");
     }
     
     if (playerSelection === options[2] && computerSelection === options[0]) {
         displayScore(playerScore, ++computerScore);
-        return result.textContent = "You lost, scissors < rock";
+        return endGame() || (result.textContent = "You lost, scissors < rock");
     }
     
     if (playerSelection === options[2] && computerSelection === options[1]) {
         displayScore(++playerScore, computerScore);
-        return result.textContent = "You won, scissors > paper";
+        return endGame() || (result.textContent = "You won, scissors > paper");
     }
 }
 
 // display the score
 function displayScore(playerScore, computerScore) {
     score.textContent = `You ` + playerScore + ' : ' + computerScore + ` Computer`;
+}
+
+// did the game end?
+function endGame() {
+    if (playerScore === 5) {
+        buttons.forEach((button) => button.removeEventListener('click', getPlayerSelection));
+        return result.textContent = "Game Over: You won!";
+    }
+    if (computerScore === 5) {
+        buttons.forEach((button) => button.removeEventListener('click', getPlayerSelection));
+        return result.textContent = "Game Over: You lost!";
+    }
 }
 
 // restart game
